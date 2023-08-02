14af289c4281c997538bbc2fc678f22c267160be
Pavlos Mastorakis <140441794+PavlosMastorakis@users.noreply.github.com>
2023-08-02 18:29:49 +0300

    Update operations.hpp

--- operations.hpp
+++ operations.hpp
@@ -19,25 +19,25 @@ void praxis(std::vector<std::string> &string1) // str1
         string1.erase(iteratorFirstPower, iteratorFirstPower + 3);
     }
 
-    int countMultDiv = count(string1.begin(), string1.end(), "*");
-    countmd += count(string1.begin(), string1.end(), "/");
-    std::string mlt = "*", dvs = "/";
+    int countMultDiv = count(string1.begin(), string1.end(), "*"); // countmp
+    countMultDiv += count(string1.begin(), string1.end(), "/");
+    // std::string mlt = "*", dvs = "/";
     
-    for (int i = 0; i < countmd; i++)
+    for (int i = 0; i < countMultDiv; i++)
     {
-        std::vector<std::string>::iterator indmin;
-        auto indexm = std::find(string1.begin(), string1.end(), mlt);
-        auto indexd = std::find(string1.begin(), string1.end(), dvs);
-        (indexm < indexd) ? (indmin = indexm) : (indmin = indexd);
-        int k = std::distance(string1.begin(), indmin);
-        double x = std::stod(string1[k - 1]);
-        double y = std::stod(string1[k + 1]);
-        double z(0);
-        (string1[k] == "*") ? (z = x * y) : (z = x / y);
-        std::string h = std::to_string(z);
-        string1.emplace(indmin - 1, h);
-        indmin = string1.begin() + k;
-        string1.erase(indmin, indmin + 3);
+        std::vector<std::string>::iterator iteratorFirstMultDiv; // indexmin
+        auto iteratorFirstMult = std::find(string1.begin(), string1.end(), "*"); // indexm
+        auto iteratorFirstDiv = std::find(string1.begin(), string1.end(), "/"); // indexd
+        (iteratorFirstMult < iteratorFirstDiv) ? (iteratorFirstMultDiv = iteratorFirstMult) : (iteratorFirstMultDiv = iteratorFirstDiv);
+        int indexFirstMultDiv = std::distance(string1.begin(), iteratorFirstMultDiv); // k
+        double firstOperand = std::stod(string1[indexFirstMultDiv - 1]); // x
+        double secondOperand = std::stod(string1[indexFirstMultDiv + 1]); // y
+        double result(0); // z
+        (string1[indexFirstMultDiv] == "*") ? (result = firstOperand * secondOperand) : (result = firstOperand / secondOperand);
+        std::string resultString = std::to_string(result); // h
+        string1.emplace(iteratorFirstMultDiv - 1, resultString);
+        iteratorFirstMultDiv = string1.begin() + indexFirstMultDiv;
+        string1.erase(iteratorFirstMultDiv, iteratorFirstMultDiv + 3);
     }
 
     int countas = count(string1.begin(), string1.end(), "+");
