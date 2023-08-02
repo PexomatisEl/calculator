4644a098516a429a9e0c6560a7a295ea4f4f8a41
Pavlos Mastorakis <140441794+PavlosMastorakis@users.noreply.github.com>
2023-08-02 18:21:45 +0300

    Update operations.hpp

--- operations.hpp
+++ operations.hpp
@@ -19,45 +19,45 @@ void praxis(std::vector<std::string> &string1) // str1
         string1.erase(iteratorFirstPower, iteratorFirstPower + 3);
     }
 
-    int countmd = count(str1.begin(), str1.end(), "*");
-    countmd += count(str1.begin(), str1.end(), "/");
+    int countMultDiv = count(string1.begin(), string1.end(), "*");
+    countmd += count(string1.begin(), string1.end(), "/");
     std::string mlt = "*", dvs = "/";
     
     for (int i = 0; i < countmd; i++)
     {
         std::vector<std::string>::iterator indmin;
-        auto indexm = std::find(str1.begin(), str1.end(), mlt);
-        auto indexd = std::find(str1.begin(), str1.end(), dvs);
+        auto indexm = std::find(string1.begin(), string1.end(), mlt);
+        auto indexd = std::find(string1.begin(), string1.end(), dvs);
         (indexm < indexd) ? (indmin = indexm) : (indmin = indexd);
-        int k = std::distance(str1.begin(), indmin);
-        double x = std::stod(str1[k - 1]);
-        double y = std::stod(str1[k + 1]);
+        int k = std::distance(string1.begin(), indmin);
+        double x = std::stod(string1[k - 1]);
+        double y = std::stod(string1[k + 1]);
         double z(0);
-        (str1[k] == "*") ? (z = x * y) : (z = x / y);
+        (string1[k] == "*") ? (z = x * y) : (z = x / y);
         std::string h = std::to_string(z);
-        str1.emplace(indmin - 1, h);
-        indmin = str1.begin() + k;
-        str1.erase(indmin, indmin + 3);
+        string1.emplace(indmin - 1, h);
+        indmin = string1.begin() + k;
+        string1.erase(indmin, indmin + 3);
     }
 
-    int countas = count(str1.begin(), str1.end(), "+");
-    countas += count(str1.begin(), str1.end(), "-");
+    int countas = count(string1.begin(), string1.end(), "+");
+    countas += count(string1.begin(), string1.end(), "-");
     std::string addi = "+", subt = "-";
     
     for (int i = 0; i < countas; i++)
     {
         std::vector<std::string>::iterator indmin;
-        auto indexa = std::find(str1.begin(), str1.end(), addi);
-        auto indexs = std::find(str1.begin(), str1.end(), subt);
+        auto indexa = std::find(string1.begin(), string1.end(), addi);
+        auto indexs = std::find(string1.begin(), string1.end(), subt);
         (indexa < indexs) ? (indmin = indexa) : (indmin = indexs);
-        int k = std::distance(str1.begin(), indmin);
-        double x = std::stod(str1[k - 1]);
-        double y = std::stod(str1[k + 1]);
+        int k = std::distance(string1.begin(), indmin);
+        double x = std::stod(string1[k - 1]);
+        double y = std::stod(string1[k + 1]);
         double z(0);
-        (str1[k] == "+") ? (z = x + y) : (z = x - y);
+        (string1[k] == "+") ? (z = x + y) : (z = x - y);
         std::string h = std::to_string(z);
-        str1.emplace(indmin - 1, h);
-        indmin = str1.begin() + k;
-        str1.erase(indmin, indmin + 3);
+        string1.emplace(indmin - 1, h);
+        indmin = string1.begin() + k;
+        string1.erase(indmin, indmin + 3);
     }
 }
