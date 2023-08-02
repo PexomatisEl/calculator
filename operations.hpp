ec5e5582543aec93067532fa969f18b4146475df
PexomatisEl <elpexom@gmail.com>
2023-08-02 19:34:49 +0300

    Update operations.hpp

--- operations.hpp
+++ operations.hpp
@@ -1,63 +1,60 @@
 const double pi = 3.14159265358979323846;
 const double e = 2.71828182845904523536;
 
-void praxis(std::vector<std::string> &string1) // str1
+void praxis( std::vector<std::string> &string1 ) 
 {
-    int countPower = count(string1.begin(), string1.end(), "^"); // countp
-    // std::string powr = "^";
+    int countPower = count( string1.begin(), string1.end(), "^" );
     
-    for (int i = 0; i < countPower; i++)
+    for ( int i = 0; i < countPower; i++ )
     {
-        auto iteratorFirstPower = std::find(string1.begin(), string1.end(), "^"); // indexp
-        int indexFirstPower = std::distance(string1.begin(), iteratorFirstPower); // k
-        double base = std::stod(string1[k - 1]); // x
-        double power = std::stod(string1[k + 1]); // y
-        double doubleResult = pow(base, power); // z
-        std::string strResulttring = std::to_string(doubleResult); // h
-        string1.emplace(indexp - 1, strResulttring);
+        auto iteratorFirstPower = std::find( string1.begin(), string1.end(), "^" ); 
+        int indexFirstPower = std::distance( string1.begin(), iteratorFirstPower ); 
+        double base = std::stod( string1[k - 1] );
+        double power = std::stod( string1[k + 1] ); 
+        double doubleResult = pow( base, power );
+        std::string strResulttring = std::to_string( doubleResult );
+        string1.emplace( indexp - 1, strResulttring );
         iteratorFirstPower = string1.begin() + indexFirstPower;
-        string1.erase(iteratorFirstPower, iteratorFirstPower + 3);
+        string1.erase( iteratorFirstPower, iteratorFirstPower + 3 );
     }
 
-    int countMultDiv = count(string1.begin(), string1.end(), "*"); // countmp
-    countMultDiv += count(string1.begin(), string1.end(), "/");
-    // std::string mlt = "*", dvs = "/";
+    int countMultDiv = count( string1.begin(), string1.end(), "*" );
+    countMultDiv += count( string1.begin(), string1.end(), "/" );
     
-    for (int i = 0; i < countMultDiv; i++)
+    for ( int i = 0; i < countMultDiv; i++ )
     {
-        std::vector<std::string>::iterator iteratorFirstMultDiv; // indmin
-        auto iteratorFirstMult = std::find(string1.begin(), string1.end(), "*"); // indexm
-        auto iteratorFirstDiv = std::find(string1.begin(), string1.end(), "/"); // indexd
-        (iteratorFirstMult < iteratorFirstDiv) ? (iteratorFirstMultDiv = iteratorFirstMult) : (iteratorFirstMultDiv = iteratorFirstDiv);
-        int indexFirstMultDiv = std::distance(string1.begin(), iteratorFirstMultDiv); // k
-        double firstOperand = std::stod(string1[indexFirstMultDiv - 1]); // x
-        double secondOperand = std::stod(string1[indexFirstMultDiv + 1]); // y
-        double doubleResult(0); // z
-        (string1[indexFirstMultDiv] == "*") ? (doubleResult = firstOperand * secondOperand) : (doubleResult = firstOperand / secondOperand);
-        std::string strResulttring = std::to_string(doubleResult); // h
-        string1.emplace(iteratorFirstMultDiv - 1, strResulttring);
+        std::vector<std::string>::iterator iteratorFirstMultDiv; 
+        auto iteratorFirstMult = std::find( string1.begin(), string1.end(), "*" ); 
+        auto iteratorFirstDiv = std::find( string1.begin(), string1.end(), "/" ); 
+        ( iteratorFirstMult < iteratorFirstDiv ) ? ( iteratorFirstMultDiv = iteratorFirstMult ) : ( iteratorFirstMultDiv = iteratorFirstDiv );
+        int indexFirstMultDiv = std::distance( string1.begin(), iteratorFirstMultDiv ); 
+        double firstOperand = std::stod( string1[indexFirstMultDiv - 1] ); 
+        double secondOperand = std::stod( string1[indexFirstMultDiv + 1] ); 
+        double doubleResult(0); 
+        ( string1[indexFirstMultDiv] == "*" ) ? ( doubleResult = firstOperand * secondOperand ) : ( doubleResult = firstOperand / secondOperand );
+        std::string strResulttring = std::to_string( doubleResult ); 
+        string1.emplace( iteratorFirstMultDiv - 1, strResulttring );
         iteratorFirstMultDiv = string1.begin() + indexFirstMultDiv;
-        string1.erase(iteratorFirstMultDiv, iteratorFirstMultDiv + 3);
+        string1.erase( iteratorFirstMultDiv, iteratorFirstMultDiv + 3 );
     }
 
-    int countAddSub = count(string1.begin(), string1.end(), "+"); // countas
-    countAddSub += count(string1.begin(), string1.end(), "-");
-    // std::string addi = "+", subt = "-";
+    int countAddSub = count( string1.begin(), string1.end(), "+" );
+    countAddSub += count( string1.begin(), string1.end(), "-" );
     
-    for (int i = 0; i < countAddSub; i++)
+    for ( int i = 0; i < countAddSub; i++ )
     {
-        std::vector<std::string>::iterator iteratorFirstAddSub; // indmin
-        auto iteratorFirstAdd = std::find(string1.begin(), string1.end(), "+"); // indexa
-        auto iteratorFirstSub = std::find(string1.begin(), string1.end(), "-"); // indexs
-        (iteratorFirstAdd < iteratorFirstSub) ? (iteratorFirstAddSub = iteratorFirstAdd) : (iteratorFirstAddSub = iteratorFirstSub);
-        int indexFirstAddSub = std::distance(string1.begin(), iteratorFirstAddSub); // k
-        double firstOperand = std::stod(string1[indexFirstAddSub - 1]); // x
-        double secondOperand = std::stod(string1[indexFirstAddSub + 1]); // y
+        std::vector<std::string>::iterator iteratorFirstAddSub; 
+        auto iteratorFirstAdd = std::find( string1.begin(), string1.end(), "+" );
+        auto iteratorFirstSub = std::find( string1.begin(), string1.end(), "-" ); 
+        ( iteratorFirstAdd < iteratorFirstSub ) ? ( iteratorFirstAddSub = iteratorFirstAdd ) : ( iteratorFirstAddSub = iteratorFirstSub );
+        int indexFirstAddSub = std::distance( string1.begin(), iteratorFirstAddSub ); 
+        double firstOperand = std::stod( string1[indexFirstAddSub - 1] );
+        double secondOperand = std::stod( string1[indexFirstAddSub + 1] );
         double doubleResult(0);
-        (string1[indexFirstAddSub] == "+") ? (doubleResult = firstOperand + secondOperand) : (doubleResult = firstOperand - secondOperand);
-        std::string strResulttring = std::to_string(doubleResult); // h
-        string1.emplace(iteratorFirstAddSub - 1, strResulttring);
+        ( string1[indexFirstAddSub] == "+" ) ? ( doubleResult = firstOperand + secondOperand ) : ( doubleResult = firstOperand - secondOperand );
+        std::string strResulttring = std::to_string( doubleResult ); // h
+        string1.emplace( iteratorFirstAddSub - 1, strResulttring );
         iteratorFirstAddSub = string1.begin() + indexFirstAddSub;
-        string1.erase(iteratorFirstAddSub, iteratorFirstAddSub + 3);
+        string1.erase( iteratorFirstAddSub, iteratorFirstAddSub + 3 );
     }
 }
